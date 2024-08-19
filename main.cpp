911847564e719386975eefcbdd497348f2b4d873
Shagee3.eg2004 <168550015+Mohamedwaleed20@users.noreply.github.com>
2024-08-19 23:52:16 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    char c ;
+    cin >> c;
+    char res = c + 1;
+    if (c == 'z')
+    {
+        cout << 'a' ;
+    }
+    else
+    {
+         cout << res ;
+    }
+
+    return 0;
+}
